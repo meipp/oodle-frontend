49d5930aff2426f18af08b5e3e06b012d7314f1b
meipp <meipp@users.noreply.github.com>
2022-11-07 22:21:04 +0100

    Add Matrix view

---
+++ src/components/Poll/View/Matrix.jsx
@@ -0,0 +1,179 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Table from "@mui/material/Table";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import SelectionButton from "../SelectionButton";
+import { useParams, useNavigate } from "react-router-dom";
+import { TableBody, TableHead } from "@mui/material";
+import { API_URL } from "../../../config";
+import Selection from "../Selection";
+
+export default function PollViewMatrix({poll, id, setLoading}) {
+  console.log("reset");
+
+  const userSelections = [];
+  for(const x of poll.x) {
+    for(const y of poll.y) {
+      userSelections.push({x, y, selection: "unknown"});
+    }
+  }
+
+  function makeUserSelection(x, y, selection) {
+    const o = userSelections.find(s => s.x === x && s.y === y);
+    if(o) {
+      o.selection = selection;
+    }
+    else {
+      userSelections.push({x, y, selection});
+    }
+  }
+
+  function mul(x, v) {
+    return v.map(vi => x*vi);
+  }
+
+  function add(v1, ...vs) {
+    const sum = [...v1];
+
+    for(const vi of vs) {
+      if(vi.length !== v1.length) {
+        throw new Error("Vectors must have the same length.");
+      }
+
+      vi.forEach((vij, j) => sum[j] += vij);
+    }
+
+    return sum;
+  }
+
+  // console.log('hello', add(mul(7, [5, 3, 6]), [1,2,3]));
+
+  function countNSelection(x, y, selection) {
+    let n = 0;
+    for(const {selections} of poll.responses ?? []) {
+      selections.find(s => s.x === x && s.y === y && s.selection === selection) && n++;
+    }
+    return n;
+  }
+
+  function mixColors(nGreen, nRed, nYellow) {
+    const n = nGreen + nRed + nYellow;
+
+    const green  = [72, 199, 142];
+    const red    = [241, 70, 104];
+    const yellow = [255, 224, 138];
+
+    // const [r, g, b] = [0, 1, 2].map(i => nGreen/n*green[i] + nRed/n*red[i] + nYellow/n*yellow[i]);
+    const [r, g, b] = add(mul(nGreen/n, green), mul(nRed/n, red), mul(nYellow/n, yellow));
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+  // function gradient(k) {
+  //   const n = poll.responses.length;
+
+  //   const color1 = [0xf0, 0x80, 0x80];
+  //   const color2 = [0x90, 0xee, 0x90];
+
+  //   const [r, g, b] = [0, 1, 2].map(i => color1[i] + (color2[i]-color1[i])*k/n);
+
+  //   // const r = 255 - k/n*255;
+  //   // const g = 0 + k/n*255;
+  //   // const b = 0;
+
+  //   return `rgb(${r}, ${g}, ${b})`;
+  // }
+
+  async function submit() {
+    const data = { name: "unknown name", selections: userSelections };
+    const url = `${API_URL}/poll/respond/${id}`;
+    console.log(`POST ${url} ${JSON.stringify(data)}`);
+
+    await axios.post(url, data);
+    setLoading(true);
+  }
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        {poll.title}
+      </Typography>
+      <Typography variant="subtitle1" color="default" sx={{ whiteSpace: "pre-line" }}>
+        {poll.description}
+      </Typography>
+      <Box component="form" noValidate sx={{ mt: 1 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell> {/* empty cell */}
+              {poll.x.map((x, i) => (
+                <TableCell align="center" key={i}>
+                  {x}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {poll.y.map((y, i) => (
+              <TableRow key={i}>
+                <TableCell>{y}</TableCell>
+                {poll.x.map((x, j) => (
+                  <TableCell
+                    key={`${i}x${j}`}
+                    sx={{
+                      // m: "10px",
+                      // backgroundColor: "green"
+                      // background: "linear-gradient(#e66465, #9198e5)"
+                      // background: "rgb(0, 0, 99.9)"
+                      // background: gradient(countNSelection(x, y, "yes"))
+                      background: mixColors(countNSelection(x, y, "yes"), countNSelection(x, y, "no"), countNSelection(x, y, "unknown"))
+                      // background: "repeating-linear-gradient(\
+                      //   135deg,\
+                      //   red,\
+                      //   red 10px,\
+                      //   green 10px,\
+                      //   green 20px,\
+                      //   blue 10px,\
+                      //   blue 20px\
+                      // )"
+                    }}
+                  >
+                    {/* {x}+{y} */}
+
+                    {countNSelection(x, y, "yes")}/{(poll.responses ?? []).length}
+                    <SelectionButton
+                        selection_id={`selection-${i}`}
+                        onUpdate={(s) => makeUserSelection(x, y, s)}
+                        // onClick={(e) => console.log(e.message)}
+                        // onUpdateState={(s) => console.log(s)}
+                    ></SelectionButton>
+                </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+
+        <Button
+          onClick={submit}
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+}
---
+++ src/components/Poll/View/Simple.jsx
@@ -0,0 +1,111 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Table from "@mui/material/Table";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import SelectionButton from "../SelectionButton";
+import { useParams, useNavigate } from "react-router-dom";
+import { TableBody, TableHead } from "@mui/material";
+import { API_URL } from "../../../config";
+import Selection from "../Selection";
+
+export default function PollViewSimple({poll, id, setLoading}) {
+  const [name, setName] = React.useState("");
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    const selections = poll.x.map((x, i) => ({
+      x,
+      selection: document.getElementById(`selection-${i}`).value || "unknown",
+    }));
+    const data = { name, selections };
+    const url = `${API_URL}/poll/respond/${id}`;
+    console.log(`POST ${url} ${JSON.stringify(data)}`);
+
+    await axios.post(url, data);
+    setLoading(true);
+  };
+
+  function makeIcon(x, selections) {
+    const selection =
+      selections.find((selection) => selection.x === x)?.selection || "unknown";
+    return <Selection state={selection}></Selection>;
+  }
+
+  return (
+    <Box
+      sx={{
+        marginTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Typography component="h1" variant="h5">
+        {poll.title}
+      </Typography>
+      <Typography variant="subtitle1" color="default" sx={{ whiteSpace: "pre-line" }}>
+        {poll.description}
+      </Typography>
+      <Box component="form" noValidate sx={{ mt: 1 }}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell> {/* empty cell */}
+              {poll.x.map((x, i) => (
+                <TableCell align="center" key={i}>
+                  {x}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(poll.responses || []).map(({ name, selections }, i) => (
+              <TableRow key={i}>
+                <TableCell>{name}</TableCell>
+                {poll.x.map((x, i) => (
+                  <TableCell key={i}>{makeIcon(x, selections)}</TableCell>
+                ))}
+              </TableRow>
+            ))}
+            <TableRow>
+              <TableCell>
+                <TextField
+                  value={name}
+                  onChange={handleChange}
+                  placeholder="Your name"
+                  sx={{ width: "8em" }}
+                ></TextField>
+              </TableCell>
+              {poll.x.map((x, i) => (
+                <TableCell key={i}>
+                  <SelectionButton
+                    selection_id={`selection-${i}`}
+                    onClick={(e) => console.log(e.message)}
+                  ></SelectionButton>
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableBody>
+        </Table>
+
+        <Button
+          onClick={submit}
+          fullWidth
+          variant="contained"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Submit
+        </Button>
+      </Box>
+    </Box>
+  );
+}
